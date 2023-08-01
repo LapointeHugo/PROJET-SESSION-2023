76a58038ba173e0cbd3e2774dc989b71282f0e02
hlapointe <1946091@cstjean.qc.ca>
2023-08-01 01:17:03 -0400

    PROGRESS TP3 (Serveur)
    Fait :
    - Debut de la section favoris 
    
    Reste 
    - Finir le code pour l'ajout d'un favoris, la suppression d'un favoris (changer le foreach pour les deux)
    - Filtre recherche (code)
    - Vue partielle
    
    Question :
    - Champs hidden id
    - Prealables
    - HttpContext marche pas
    - Faut que se soit une table??

--- ProjetSessionHL/ProjetSessionHL/Controllers/FavorisController.cs
+++ ProjetSessionHL/ProjetSessionHL/Controllers/FavorisController.cs
@@ -1,12 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetSessionHL.Models;
+using System.Collections.Generic;
 
 namespace ProjetSessionHL.Controllers
 {
     public class FavorisController : Controller
     {
+        private BaseDeDonnees _baseDeDonnees { get; set; }
+
+        public FavorisController(BaseDeDonnees baseDeDonnees)
+        {
+            _baseDeDonnees = baseDeDonnees;
+        }
+
         public IActionResult Index()
         {
+            var enfantsIDs = HttpContext.Session.Get<List<int>>("enfantsIDs");
+            if (enfantsIDs == null)
+            {
+                enfantsIDs = new List<int>();
+            }
+
+            var enfantsDeLaBD = _baseDeDonnees.Enfants.Where(e => enfantsIDs.Contains(e.Id)).ToList();
+
+            return View(_baseDeDonnees.Enfants.Take(3).ToList());
+        }
+
+        public IActionResult AjouterUnEnfant(int id)
+        {
+            var enfantsIDs = HttpContext.Session.Get<List<int>>("enfantsIDs");
+            if (enfantsIDs == null)
+            {
+                enfantsIDs = new List<int>();
+            }
+
+            foreach (var item in enfantsIDs)
+            {
+                if (id != item.Id)
+                {
+                    enfantsIDs.Add(id);
+                }
+            }
+
+            //Sauvegarder la liste modifier dans la session
+           
             return View("Index");
+        }
+
+        public IActionResult SupprimerUnEnfant(int id)
+        {
+            var enfantsIDs = HttpContext.Session.Get<List<int>>("enfantsIDs");
+            if (enfantsIDs == null)
+            {
+                enfantsIDs = new List<int>();
+            }
+
+            foreach (var item in enfantsIDs)
+            {
+                if (id == item.Id)
+                {
+                    enfantsIDs.Remove(id);
+                }
+            }
+
+            //Sauvegarder la liste modifier dans la session
+
+            return View("Index");   
         }
     }
 }
