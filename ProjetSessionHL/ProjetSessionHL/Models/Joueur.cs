4adeadf44d6a071d8bc7635417d23d57cc2ce01e
Rapid <1946091@cegepmontpetit.ca>
2023-08-21 20:23:03 -0400

    FCT Ajout Table Joueur et Tournoi
    - Créer modèle pour Joueur et Tournoi
    - Ajouter le dbset pour chacun
    - Faire du seed pour ajouter les données
    - Faire les liaisons plusieurs à plusieurs entre les deux
    - Faite la migration et le update

---
+++ ProjetSessionHL/ProjetSessionHL/Models/Joueur.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetSessionHL.Models
+{
+    public class Joueur
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "ValidationRequired")]
+        [MaxLength(30, ErrorMessage = "MaxCaractersValidation")]
+        public string Name { get; set; }
+
+        [Display(Name = "Game")]
+        [MaxLength(100, ErrorMessage = "{1} caracteres maximum!")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "ValidationRequired")]
+        public string Game { get; set; }
+
+        [Display(Name = "Age")]
+        [Range(16,50)]
+        public int Age { get; set; }
+
+        public ICollection<Tournoi> Tournois { get; set; }
+    }
+}
---
+++ ProjetSessionHL/ProjetSessionHL/Models/Tournoi.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetSessionHL.Models
+{
+    public class Tournoi
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "ValidationRequired")]
+        [MaxLength(30, ErrorMessage = "MaxCaractersValidation")]
+        public string Name { get; set; }
+
+        [Display(Name = "Location")]
+        [MaxLength(100, ErrorMessage = "{1} caracteres maximum!")]
+        public string Location { get; set; }
+
+        [Display(Name = "Prize Pool")]
+        public float PrizePool { get; set; }
+
+        public ICollection<Joueur> Joueurs { get; set; }
+    }
+}
