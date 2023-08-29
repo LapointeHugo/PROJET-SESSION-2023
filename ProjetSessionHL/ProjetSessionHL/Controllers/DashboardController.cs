545610bb5209dad3ef6a295cc3d4263d9805fe37
Rapid <1946091@cegepmontpetit.ca>
2023-08-29 18:40:43 -0400

    FCT Ajout_ControllerDashboard
    Ajout du controlleur pour les dashboard

---
+++ ProjetSessionHL/ProjetSessionHL/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using ProjetSessionHL.Data;
+using ProjetSessionHL.Models;
+
+namespace ProjetSessionHL.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ProjetSessionDbContext _baseDeDonnees;
+
+        public DashboardController(ProjetSessionDbContext baseDeDonnees)
+        {
+            _baseDeDonnees = baseDeDonnees;
+        }
+
+        public IActionResult Admin()
+        {
+            List<StatistiquesDonnees> objList = _baseDeDonnees.StatistiquesDonnees.ToList();
+            return View(objList);
+        }
+
+        public IActionResult Coach()
+        {
+            List<StatistiquesDonnees> objList = _baseDeDonnees.StatistiquesDonnees.ToList();
+            return View(objList);
+        }
+    }
+}
