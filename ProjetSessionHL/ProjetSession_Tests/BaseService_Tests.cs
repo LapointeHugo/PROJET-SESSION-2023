b93cc759585107f1c1cd309f274dc9942fdb572d
hlapointe <1946091@cstjean.qc.ca>
2023-09-05 05:00:27 -0400

    FCT Ajout_Tests_Unitaires_Controlleurs_Services
    Ajouter les tests pour controlleur mas erreur de ma par dans le code pas capable de trouver donc mis en commentaires et pour les services ont fais des test sur quelle services.

---
+++ ProjetSessionHL/ProjetSession_Tests/BaseService_Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetSessionHL.Data;
+using ProjetSessionHL.Services;
+using ProjetSessionHL.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetSession_Tests
+{
+    public class BaseService_Tests
+    {
+        private DbContextOptions<ProjetSessionDbContext> SetUpInMemory(string uniqueName)
+        {
+            //var options = new DbContextOptionsBuilder<ProjetSessionDbContext>().UseInMemoryDatabase(uniqueName).Options;
+            //SeedInMemoryStore(options);
+            //return options;
+        }
+    }
+}
